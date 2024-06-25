08f2a59e1fb4b4a1f50a512460de9f781a5fd0e3
AoiTomiyama <TadoO.o2209@gmail.com>
2024-06-25 21:58:52 +0900

    別プロジェクトから途中経過を輸送。

---
+++ Assets/BulletHell/HealthController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthController : MonoBehaviour
+{
+    [Header("HPの量(-1で体力∞)")]
+    [SerializeField]
+    int _health = 5; 
+
+    [Header("体力ゲージ1つおきの幅")]
+    [SerializeField]
+    int _healthPadding = 5;
+
+    [Header("体力ゲージの見た目")]
+    [SerializeField]
+    GameObject _healthPrefab;
+
+    GameObject[] _healthBar;
+
+    private void Start()
+    {
+        if (_health != -1)
+        {
+            _healthBar = new GameObject[_health];
+            for (int i = 0; i < _health; i++)
+            {
+                _healthBar[i] = Instantiate(_healthPrefab, transform);
+                _healthBar[i].transform.position = new Vector2(this.transform.position.x + i * _healthPadding, this.transform.position.y);
+            }
+        }
+        else
+        {
+            GetComponent<Text>().text = "∞";
+        }
+    }
+    public void RemoveHealth(int damage)
+    {
+        if (_health != -1)
+        {
+            if (_health - damage < 0)
+            {
+                Gameover();
+                return;
+            }
+            else
+            {
+                for (int i = _health; i > _health - damage; i--)
+                {
+                    Destroy(_healthBar[i - 1]);
+                }
+                _health -= damage;
+                Debug.Log($"Bullet Hit! Took {damage} damage! Remaining health is {_health} !");
+            }
+            if (_health == 0)
+            {
+                Gameover();
+            }
+        }
+        else
+        {
+            Debug.Log("Bullet Hit!");
+        }
+    }
+    void Gameover()
+    {
+        Debug.Log("Gameover");
+        Time.timeScale = 0;
+    }
+}
---
+++ Assets/BulletHell/RandomDirection.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 指定秒数毎に向きをランダムな方向へ変える。
+/// </summary>
+public class RandomDirection : MonoBehaviour
+{
+    [Header("方向を変える周期")]
+    [SerializeField]
+    float _waitTime;
+
+    [Header("方向の最大値")]
+    [SerializeField]
+    float _maxAngular = 360;
+
+    [Header("方向の最小値")]
+    [SerializeField]
+    float _minAngular = 0;
+
+    private void Start()
+    {
+        StartCoroutine(RandomRotateRepeat());
+    }
+
+    IEnumerator RandomRotateRepeat()
+    {
+        while (true)
+        {
+            float randomZRotation = Random.Range(_minAngular, _maxAngular);
+            transform.rotation = Quaternion.Euler(0f, 0f, randomZRotation);
+            yield return new WaitForSeconds(_waitTime);
+        }
+    }
+}

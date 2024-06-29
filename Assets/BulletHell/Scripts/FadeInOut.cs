57b5393dece5687c89054b42729dffaed3719253
AoiTomiyama <TadoO.o2209@gmail.com>
2024-06-29 19:16:30 +0900

    AssetStoreToolsがちゃんと.gitignoreされてないのに今更気づいたので修正、タイトル・ゲームオーバー画面のフォントをデフォルトから変更、タイトルの設定ボタンを右下に移動、DOTweenの導入。

--- Assets/BulletHell/Scripts/FadeInOut.cs
+++ Assets/BulletHell/Scripts/FadeInOut.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,26 +11,22 @@ public class FadeInOut : MonoBehaviour
     [Header("フェードインにかかる時間")]
     [SerializeField]
     float _fadeInTimer = 1f;
+
+    Tween _tween;
     /// <summary>Imageコンポーネントを取得</summary>
     Image _image;
     void Start()
     {
         _image = GetComponent<Image>();
-        StartCoroutine(FadeOut());
-    }
-    IEnumerator FadeOut()
-    {
         _image.enabled = true;
         var color = _image.color;
-        var elapsedTime = 0f;
-        while (elapsedTime < _fadeInTimer)
-        {
-            var elapsedRate = Mathf.Min(elapsedTime / _fadeInTimer, 1f);
-            color.a = 1.0f - elapsedRate;
-            yield return null;
-            elapsedTime += Time.deltaTime;
-            _image.color = color;
-        }
-        _image.enabled = false;
+        color.a = 1f;
+        _image.color = color;
+        _tween = _image.DOFade(0, _fadeInTimer);
+    }
+
+    private void OnDisable()
+    {
+        _tween.Kill();
     }
 }

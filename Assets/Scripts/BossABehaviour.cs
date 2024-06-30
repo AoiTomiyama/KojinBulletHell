823df325d690883544579cb6666931cf5234d329
AoiTomiyama <TadoO.o2209@gmail.com>
2024-06-30 12:59:24 +0900

    ボスAの見た目部分（3D）と当たり判定&位置部分（2D）を分離させた。攻撃パターン1のコード部分を整理。

--- Assets/Scripts/BossABehaviour.cs
+++ Assets/Scripts/BossABehaviour.cs
@@ -12,24 +12,34 @@ public class BossABehaviour : MonoBehaviour
     Transform[] _pos;
     /// <summary>Tween完了前にシーン移動した際にKillできるように保存</summary>
     Tween _tween;
+    /// <summary>攻撃パターンを入れる。完了前にシーン移動した際にKillできるように保存</summary>
+    Sequence _seq;
     /// <summary>行動パターンの保管</summary>
     Action[] _actions;
-    ParticleSystem _ps;
+    /// <summary>ボスの見た目部分。</summary>
+    GameObject _bossCube;
+    /// <summary>開始時の位置</summary>
+    Vector2 _startPos;
+    /// <summary>弾幕を発生させる位置</summary>
+    Transform _particleTr;
     private void Start()
     {
+        _startPos = this.transform.position;
+        _particleTr = this.transform.Find("ParticlePosition").transform;
+        _bossCube = this.transform.Find("BossCube").gameObject;
         _pos = GameObject.Find("Positions").transform.GetComponentsInChildren<Transform>();
         _actions = new Action[1];
         _actions[0] = AttackPatternOne;
-        _tween = transform.DORotate(new Vector3(Random.Range(0, 200), Random.Range(0, 200), Random.Range(0, 200)), 1.5f, RotateMode.FastBeyond360).
+        _tween = _bossCube.transform.DORotate(new Vector3(Random.Range(0, 200), Random.Range(0, 200), Random.Range(0, 200)), 1.5f, RotateMode.FastBeyond360).
             SetLoops(-1, LoopType.Incremental).
             SetEase(Ease.Linear).OnStart(() =>
             {
-                transform.DOMove(new Vector3(-transform.position.x, transform.position.y), 3).
+                this.transform.DOMove(new Vector2(-_startPos.x, _startPos.y), 3).
                 SetLoops(2, LoopType.Yoyo).
                 SetEase(Ease.InOutQuad).
                 OnComplete(() =>
                 {
-                    transform.DOPause();
+                    _bossCube.transform.DOPause();
                     _actions[Random.Range(0, _actions.Length)].Invoke();
                 });
             }
@@ -37,35 +47,48 @@ public class BossABehaviour : MonoBehaviour
     }
     private void AttackPatternOne()
     {
-        var seq = DOTween.Sequence();
-        seq.Append(transform.DORotate(new Vector3(0, 720, 0), 0.5f, RotateMode.FastBeyond360).
-            OnStart(() => transform.DOMove(_pos[1].position, 0.5f))
+        float duration = 5f;
+        ParticleSystem ps = default;
+        _seq = DOTween.Sequence();
+        _seq.Append(
+            _bossCube.transform.DORotate(new Vector3(0, 720, 0), 0.5f, RotateMode.FastBeyond360).
+            OnStart(() => this.transform.DOMove(_pos[1].position, 0.5f))
             );
-        seq.Append(transform.DOMoveX(-transform.position.x, 5).OnStart(() =>
-                    {
-                        _ps = Instantiate(_particles[0], transform.position, Quaternion.identity, this.transform).GetComponent<ParticleSystem>();
-                        transform.DORotate(new Vector3(0, 0, 720), 5, RotateMode.FastBeyond360);
-                    }
-                ).OnComplete(() =>
+        _seq.Append(
+            this.transform.DOMove(new Vector2(-this.transform.position.x, this.transform.position.y), duration).
+            SetEase(Ease.InOutSine).
+            OnStart(() =>
                 {
-                    var emission = _ps.emission;
+                    ps = Instantiate(_particles[0], _particleTr.position, Quaternion.identity, _particleTr).GetComponent<ParticleSystem>();
+                    _bossCube.transform.DORotate(new Vector3(0, 1080, 0), duration, RotateMode.FastBeyond360)
+                    .SetEase(Ease.InOutSine);
+                }
+            ).
+            OnComplete(() =>
+                {
+                    var emission = ps.emission;
                     emission.enabled = false;
-                    Destroy(_ps.gameObject, 5);
-                    transform.DOPlay();
-                    transform.DOMove(new Vector3(-transform.position.x, transform.position.y), 3).
-                        SetLoops(2, LoopType.Yoyo).
-                        SetEase(Ease.InOutQuad).
-                        OnComplete(() =>
-                        {
-                            transform.DOPause();
-                            _actions[Random.Range(0, _actions.Length)].Invoke();
-                        });
-                })
-            );
+                    _particleTr.DetachChildren();
+                    Destroy(ps.gameObject, duration);
+                    _bossCube.transform.DOPlay();
+                }
+            ));
+        _seq.Append(this.transform.DOMove(_startPos, 0.5f));
+        _seq.Append(
+            this.transform.DOMove(new Vector2(-_startPos.x, _startPos.y), 3).
+            SetLoops(2, LoopType.Yoyo).
+            SetEase(Ease.InOutQuad).
+            OnComplete(() =>
+                {
+                    _bossCube.transform.DOPause();
+                    _actions[Random.Range(0, _actions.Length)].Invoke();
+                }
+            ));
     }
 
     private void OnDisable()
     {
         _tween.Kill();
+        _seq.Kill();
     }
 }
